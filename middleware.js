7f2d920160dc58638ab5347cf223abde0bd1cc2e
JungSik Jeong <wndtlr1024@gmail.com>
2023-05-26 21:32:46 +0900

    .

--- middleware.js
+++ middleware.js
@@ -2,29 +2,29 @@ import { getToken } from 'next-auth/jwt';
 const { NextResponse } = require('next/server');
 
 export async function middleware(request) {
-  if (request.nextUrl.pathname.startsWith('/write')) {
-    const session = await getToken({ req: request });
+  // if (request.nextUrl.pathname.startsWith('/write')) {
+  //   const session = await getToken({ req: request });
 
-    if (session === null) {
-      return NextResponse.redirect(
-        'http://localhost:3000/api/auth/signin',
-        request.url
-      );
-    }
-  }
+  //   if (session === null) {
+  //     return NextResponse.redirect(
+  //       'http://localhost:3000/api/auth/signin',
+  //       request.url
+  //     );
+  //   }
+  // }
 
-  if (request.nextUrl.pathname.startsWith('/register')) {
-    if (request.cookies.has('visited') === false) {
-      const response = NextResponse.next();
+  // if (request.nextUrl.pathname.startsWith('/register')) {
+  //   if (request.cookies.has('visited') === false) {
+  //     const response = NextResponse.next();
 
-      response.cookies.set({
-        name: 'visited',
-        value: 'true',
-        maxAge: 3600,
-        httpOnly: true,
-      });
-      return response;
-    }
-  }
+  //     response.cookies.set({
+  //       name: 'visited',
+  //       value: 'true',
+  //       maxAge: 3600,
+  //       httpOnly: true,
+  //     });
+  //     return response;
+  //   }
+  // }
   return NextResponse.next();
 }
